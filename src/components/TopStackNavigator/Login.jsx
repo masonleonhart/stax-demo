7e1a646607ee3ae5131819389b5533a5fd0b4100
masonleonhart <mason.leonhart@gmail.com>
2021-12-02 18:42:12 -0600

    threw login function into useEffect so it runs on succesful render of page, added catches for error and cancel response types from expo-auth-session, and the post to data service, decoding of jwt id token, and sending access token only happens on response type of success from expo-auth-session

--- src/components/TopStackNavigator/Login.jsx
+++ src/components/TopStackNavigator/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useIsFocused } from "@react-navigation/native";
 import * as AuthSession from "expo-auth-session";
@@ -7,14 +7,10 @@ import axios from "axios";
 
 import config from "../../redux/sagas/server.config";
 
-import { Image, StyleSheet, View } from "react-native";
-
-import { Appbar } from "react-native-paper";
+import { Image, StyleSheet } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
 
-import SharedStyles from "../reusedComponents/SharedStyles";
-import MyButton from "../reusedComponents/MyButton";
 import StaxLogo from "../../../assets/StaxLogoVerticleWhiteNew.png";
 import EmptyStateView from "../reusedComponents/EmptyStateView";
 
@@ -28,81 +24,102 @@ function Login({ navigation }) {
   const auth0Domain = "dev-ndj3izs8.us.auth0.com";
   const auth0ClientId = "SXnendrTRmYP6v3oWkMHEK3QHQTVL8sz";
 
-  // on sign in press, opens a web view to auth0 to login, on successful return from auth0 save the access
-  // token into redux store for further use, decode the jwt id token, set the state of user info with the
-  // gathered data from auth0, and send the data to the backend to create a new user or update the access token
-  // then navigates to the landing page if successful login
+  // When the page finishes rendering, open the webview to run login
 
-  const loginWithAuth0 = async () => {
-    // Retrieve the redirect URL, add this to the callback URL list
-    // of your Auth0 application.
-    const useProxy = Platform.select({ web: false, default: true });
-    const redirectUri = AuthSession.makeRedirectUri({ useProxy });
+  useEffect(() => {
+    // on sign in press, opens a web view to auth0 to login, on successful return from auth0 save the access
+    // token into redux store for further use, decode the jwt id token, set the state of user info with the
+    // gathered data from auth0, and send the data to the backend to create a new user or update the access token
+    // then navigates to the landing page if successful login
 
-    const toQueryString = (params) =>
-      "?" +
-      Object.entries(params)
-        .map(
-          ([key, value]) =>
-            `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-        )
-        .join("&");
+    const loginWithAuth0 = async () => {
+      // Retrieve the redirect URL, add this to the callback URL list
+      // of your Auth0 application.
+      const useProxy = Platform.select({ web: false, default: true });
+      const redirectUri = AuthSession.makeRedirectUri({ useProxy });
 
-    // Structure the auth parameters and URL
-    const params = {
-      prompt: "login",
-      client_id: auth0ClientId,
-      redirect_uri: redirectUri,
-      // response_type:
-      // id_token will return a JWT token with the profile as described on the scope
-      // token will return access_token to use with further api calls
-      response_type: "token id_token",
-      nonce: "nonce", // ideally, this will be a random value
-      scope: "openid profile email",
+      const toQueryString = (params) =>
+        "?" +
+        Object.entries(params)
+          .map(
+            ([key, value]) =>
+              `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+          )
+          .join("&");
+
+      // Structure the auth parameters and URL
+      const params = {
+        prompt: "login",
+        client_id: auth0ClientId,
+        redirect_uri: redirectUri,
+        // response_type:
+        // id_token will return a JWT token with the profile as described on the scope
+        // token will return access_token to use with further api calls
+        response_type: "token id_token",
+        nonce: "nonce", // ideally, this will be a random value
+        scope: "openid profile email",
+      };
+
+      const queryParams = toQueryString(params);
+      const authUrl = `https://${auth0Domain}/authorize${queryParams}`;
+
+      let response;
+      let decodedIdToken;
+
+      try {
+        response = await AuthSession.startAsync({
+          authUrl,
+          showInRecents: true,
+        });
+
+        if (response.type === "success") {
+          decodedIdToken = jwtDecode(response.params.id_token);
+
+          dispatch({
+            type: "SET_ACCESS_TOKEN",
+            payload: response.params.access_token,
+          });
+        } else if (response.type === "cancel") {
+          loginWithAuth0();
+        } else if (response.type === "error") {
+          console.log("error in validating user from auth0", response);
+        }
+      } catch (error) {
+        console.log("error in validating user from auth0", error);
+      }
+
+      console.log({
+        access_token: userStore.userAccessToken,
+        first_name: decodedIdToken.given_name,
+        last_name: decodedIdToken.family_name,
+        email: decodedIdToken.email,
+      });
+
+      if (response.type === "success") {
+        try {
+          await axios
+            .post(`${config.serverAddress}/api/v1/authenticate-user`, {
+              access_token: userStore.userAccessToken,
+              first_name: decodedIdToken.given_name,
+              last_name: decodedIdToken.family_name,
+              email: decodedIdToken.email,
+            })
+            .then(() => navigation.navigate("Landing"));
+        } catch (error) {
+          console.log("error in sending userInfo to data service", error);
+        }
+      }
     };
 
-    const queryParams = toQueryString(params);
-    const authUrl = `https://${auth0Domain}/authorize${queryParams}`;
-
-    let response;
-
-    try {
-      response = await AuthSession.startAsync({
-        authUrl,
-        showInRecents: true,
-      });
-    } catch (error) {
-      console.log("error in validating user from auth0", error);
-    }
-
-    const decodedIdToken = jwtDecode(response.params.id_token);
-
-    dispatch({
-      type: "SET_ACCESS_TOKEN",
-      payload: response.params.access_token,
-    });
-
-    try {
-      await axios
-        .post(`${config.serverAddress}/api/v1/authenticate-user`, {
-          access_token: userStore.userAccessToken,
-          first_name: decodedIdToken.given_name,
-          last_name: decodedIdToken.family_name,
-          email: decodedIdToken.email,
-        })
-        .then(() => navigation.navigate("Landing"));
-    } catch (error) {
-      console.log("error in sending userInfo to data service", error);
-    }
-  };
+    loginWithAuth0();
+  }, []);
 
   const styles = StyleSheet.create({
     linearGradient: {
       height: "100%",
       width: "100%",
-    },
-    appbar: {
-      backgroundColor: "transparent",
+      flex: 1,
+      justifyContent: "center",
     },
     staxLogo: {
       height: "25%",
@@ -128,17 +145,7 @@ function Login({ navigation }) {
       colors={["#FEBFB0", "#FE7452", "#FE3F12"]}
       style={styles.linearGradient}
     >
-      <Appbar style={styles.appbar} />
-
       <Image source={StaxLogo} resizeMode="contain" style={styles.staxLogo} />
-
-      <View style={SharedStyles.container}>
-        <MyButton
-          onPress={loginWithAuth0}
-          text="Log in with Auth0"
-          style={styles.myButton}
-        />
-      </View>
     </LinearGradient>
   );
 }
